20b66dc9668596fd2cbeb35e18aa9521c89b31bc
snowleopardAlexa <alexaslomski@gmail.com>
2021-04-18 18:55:13 -0400

    add checkbox with useReducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import UserAge from './components/UserAge';
+import Checkbox from './components/Checkbox';
 
 function App() {
   return (
     <div className="App">
       <h1>Questionnaire</h1>
       <UserAge />
+      <Checkbox />
     </div>
   );
 }
---
+++ src/components/Checkbox.js
@@ -0,0 +1,22 @@
+import React, { useReducer } from 'react';
+
+function Checkbox() {
+    const [checked, toggle] = useReducer(
+         (checked) => !checked,
+         false
+    );
+
+  return (
+      <>
+        <input 
+          type="checkbox" 
+          value={checked} 
+          onChange={(toggle)}
+        />
+          <p>{checked ? "checked" : "not checked"}</p>
+      </>
+  )
+}
+
+
+export default Checkbox;
