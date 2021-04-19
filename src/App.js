e5e4c3c9a821e31430e4d4c6123b5830732bed44
snowleopardAlexa <alexaslomski@gmail.com>
2021-04-18 20:15:10 -0400

    fetch data from github api

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import UserAge from './components/UserAge';
 import Checkbox from './components/Checkbox';
+import FetchData from './components/FetchData';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <h1>Questionnaire</h1>
       <UserAge />
       <Checkbox />
+      <FetchData />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App login="snowleopardAlexa"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
