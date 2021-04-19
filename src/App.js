81e67fa5ceb6c12d00e60c247413355e55e97ca5
snowleopardAlexa <alexaslomski@gmail.com>
2021-04-18 20:53:54 -0400

    react-router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import './App.css';
-import UserAge from './components/UserAge';
-import Checkbox from './components/Checkbox';
-import FetchData from './components/FetchData';
+import { Routes, Route } from 'react-router-dom';
+import {
+  Home,
+  About,
+  Events,
+  Contact
+} from './components/pages/Pages';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Questionnaire</h1>
-      <UserAge />
-      <Checkbox />
-      <FetchData />
+    <div>
+    <h2>Questionnaire</h2>
+    <Routes>
+     <Route path="/" element={<Home />}/>
+     <Route path="/about" element={<About />}/>
+     <Route path="/events" element={<Events />}/>
+     <Route path="/contact" element={<Contact />}/>
+    </Routes>
     </div>
   );
 }
